8519adece250b423054f3ed397e2311fd37f2310
faar2002 <faar2002@gmail.com>
2026-08-10 13:37:50 -0400

    Primer comit

---
+++ otp_service/settings/prod.py
@@ -0,0 +1,9 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['midominio.com', 'www.midominio.com']
+
+# Medidas de seguridad avanzadas para producción
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
---
+++ otp_service/settings/qa.py
@@ -0,0 +1,4 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['qa.midominio.com', '127.0.0.1']
